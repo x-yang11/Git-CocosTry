6e8748ada4b0b49ce4b01b79e7557ea99a651ee3
Xiao Yang <x_yang11@163.com>
2014-12-25 18:43:34 +0800

    add some django examples

--- F_Code/inoutViewer/inoutViewer/urls.py
+++ F_Code/inoutViewer/inoutViewer/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from view import inoutviewer
+from inoutViewer.view import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'inoutViewer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^inoutviewer/', inoutviewer),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', inoutviewer_hello),
+    url(r'^time/$', inoutviewer_time),
+    url(r'^time/plus/(\d{1,3})/$', inoutviewer_time_offset),
+    url(r'^thanks/$', inout_template),
 )
--- F_Code/inoutViewer/inoutViewer/view.py
+++ F_Code/inoutViewer/inoutViewer/view.py
@@ -1,4 +1,48 @@
 from django.http import HttpResponse
+from django.template import Template, Context
+import datetime
 
-def inoutviewer(req):
-	return HttpResponse("<h1>Hello World!</h1>")
+def inoutviewer_hello(req):
+	return HttpResponse("Hello World!")
+
+def inoutviewer_time(req):
+	nowtime = datetime.datetime.now()
+	html = "<html><body>It is now %s.</body></html>" % nowtime
+#	return HttpResponse("Hello World!")
+	return HttpResponse(html)
+
+def inoutviewer_time_offset(req, offset):
+	offset = int(offset)
+	nowtime = datetime.datetime.now()
+	assert False
+	dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
+	html = "<html><body>In %s hours, It is %s.</body></html>" % (offset, dt)
+#	return HttpResponse("Hello World!")
+	return HttpResponse(html)
+
+def inout_template(req):
+	
+	raw_template = """<p>Dear {{ person_name }},</p>
+	
+	 <p>Thanks for placing an order from {{ company }}. It's scheduled to
+	 ship on {{ ship_date|date:"F j, Y" }}.</p>
+	
+	 {% if ordered_warranty %}
+	 <p>Your warranty information will be included in the packaging.</p>
+	 {% else %}
+	 <p>You didn't order a warranty, so you're on your own when
+	 the products inevitably stop working.</p>
+	 {% endif %}
+	
+	 <p>Sincerely,<br />{{ company }}</p>"""
+
+	 t = Template(raw_template)
+	 c = Context({'person_name': 'John Smith',
+	 	'company': 'Outdoor Equipment',
+	 	'ship_date': datetime.date(2009, 4, 2),
+	 	'ordered_warranty': False
+	 	})
+	 html = t.render(c)
+
+	 return HttpResponse(html)
+
